6d8821984117586cacb0c6ca6802bfd27c8ccb1e
Shackles Butterfly <Spicely@outlook.com>
2020-04-17 11:01:59 +0800

    []

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { TabBar, Icon } from 'components'
+import { TabBar, Icon, Toast } from 'components'
 import Home from '../Home'
 import Mall from '../Mall'
 import Shop from '../Shop'
@@ -9,6 +9,9 @@ import { SET_USERINFO_DATA, SET_SELECTED_DATA, SET_CONFIG_DATA, SET_CONFIGWEB_DA
 import { DispatchProp, connect } from 'react-redux'
 import { IInitState, IGlobal } from 'src/store/state'
 import { RouteComponentProps } from 'react-router-dom'
+import { Modal } from 'antd-mobile'
+
+const prompt = Modal.prompt;
 
 interface IProps {
     selected: number
@@ -61,12 +64,17 @@ class Index extends Component<IProps & DispatchProp & RouteComponentProps, any> 
     private getUserInfo = async () => {
         try {
             const { dispatch } = this.props
-            const data = await http('wxapp/login/userInfo')
+            const data = await http('/wxapp/login/userInfo')
             const config = await http('/wxapp/Wxpay/configWeb')
             const configWeb = await http('/wxapp/index/web_config')
             dispatch({ type: SET_CONFIG_DATA, data: config.data })
             dispatch({ type: SET_USERINFO_DATA, data: data.data })
             dispatch({ type: SET_CONFIGWEB_DATA, data: configWeb.data })
+            if (data.data.type === 3 && !data.data.remcd) {
+                prompt('请填写邀请码', '', [
+                    { text: '确认', onPress: this.handleBind },
+                ], 'default', '', ['邀请码'])
+            }
         } catch (e) {
             console.log(e)
             console.log('----------用户没有登录-------------')
@@ -80,6 +88,22 @@ class Index extends Component<IProps & DispatchProp & RouteComponentProps, any> 
             return
         }
         dispatch({ type: SET_SELECTED_DATA, data: field })
+    }
+
+    private handleBind = async (value: string) => {
+        try {
+            const { msg } = await http('/wxapp/login/bindUpuser', {
+                remcd: value
+            })
+            Toast.info({
+                content: msg
+            })
+        } catch (data) {
+            Toast.info({
+                content: data.msg || '服务器繁忙,请稍后再试'
+            })
+            throw new data.msg
+        }
     }
 }
 
